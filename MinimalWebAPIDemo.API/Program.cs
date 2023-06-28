2a11170018a6ca53a7a55a9b008a6d47dc18d9bf
kartik <kartik.kumar53@gmail.com>
2023-06-28 16:24:12 +0530

    clean code or move to all get post delete put into a extension method.#

--- MinimalWebAPIDemo.API/Program.cs
+++ MinimalWebAPIDemo.API/Program.cs
@@ -1,3 +1,4 @@
+using MinimalWebAPIDemo.API;
 using MinimalWebAPIDemo.API.Model;
 using MinimalWebAPIDemo.API.Services;
 
@@ -17,43 +18,9 @@ var app = builder.Build();
 
 #region direct use endpoint on programe.cs file
 
+app.MapBlogModelEndPoints();
 
-//// Configure routes
-app.MapGet("/api/blog", (IBlogService todoService) =>
-{
-    var todoItems = todoService.GetAll();
-    return Results.Ok(todoItems);
-});
-
-app.MapGet("/api/blog/{id}", (IBlogService todoService, int id) =>
-{
-    var todoItem = todoService.GetById(id);
-    if (todoItem == null)
-    {
-        return Results.NotFound();
-    }
-    return Results.Ok(todoItem);
-});
-
-app.MapPost("/api/blog", async (IBlogService todoService, Blog todoItem) =>
-{
-    var createdTodoItem = todoService.Create(todoItem);
-    return Results.Created($"/api/todo/{createdTodoItem.Id}", createdTodoItem);
-});
-
-app.MapPut("/api/blog/{id}", async (IBlogService todoService, int id, Blog updatedTodoItem) =>
-{
-    todoService.Update(id, updatedTodoItem);
-    return Results.NoContent();
-});
-
-app.MapDelete("/api/blog/{id}", async (IBlogService todoService, int id) =>
-{
-    todoService.Delete(id);
-    return Results.NoContent();
-});
-
-#endregion
+    #endregion
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
